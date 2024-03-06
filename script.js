91dd5632d9fecc3e8312d247aae38fb4d0f2f516
Nate Meinhold <meinholdnath@seattleu.edu>
2024-03-05 16:28:25 -0800

    updating weather app

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-//TO DO, Get API Key to replace placeholder
-const apiKey = "API_KEY";
+//create and place API Key from Weather Map
+const apiKey = "7500887a8169c25dbba8293ea48ce39f";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-//Selecting the elements
+//Collect all elements
 const locationInput = document.getElementById("locationInput");
 const searchButton = document.getElementById("searchButton");
 const locationElement = document.getElementById("location");
 const temperatureElement = document.getElementById("temperature");
 const descriptionElement = document.getElementById("description");
 
-//Add the event listener for click
-searchButton.addEventListener("click", () => {
+//Add click event for search
+searchButton.addEventListener("click", function () {
   const location = locationInput.value;
   if (location) {
     fetchWeather(location);
   }
+  //Removes the city from the search bar after the data has appeared
+  locationInput.value = "";
 });
 
-//API call (Using Fetch)
+//API call using Fetch
 function fetchWeather(location) {
   const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      //the data, think about adding temp in F as well as C if possible
       locationElement.textContent = data.name;
       temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
       descriptionElement.textContent = data.weather[0].description;
     })
+
+    //here is where the fetch error is..think about adding error for user to see
     .catch((error) => {
       console.error("Error fetching weather data:", error);
     });
